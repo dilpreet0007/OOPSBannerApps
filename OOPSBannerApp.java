ef1d5b3248b5cf3cdd31bfe5b3be3352e13d66cf
dilpreet0007 <dilpreet.sandhu2004@gmail.com>
2026-02-18 16:05:11 +0530

    UC5 code added to UC5 branch

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,8 @@
 
-public class Practice{
+public class OOPSBannerApp{
 	public static void main(String[] args){
-		String[] arr = new String[7];
+		String[] arr = {"   ****      *****   "," **    **    **      "," **    **    **      "," **    **    *****   "," **    **       **   "," **    **       **   ","   ****      *****   "};
 		
-		arr[0] = String.join(" ", "   ****   ", "   *****   ");
-		arr[1] = String.join(" ", " **    ** ", "   **      ");
-		arr[2] = String.join(" ", " **    ** ", "   **      ");
-		arr[3] = String.join(" ", " **    ** ", "   *****   ");
-		arr[4] = String.join(" ", " **    ** ", "      **   ");
-		arr[5] = String.join(" ", " **    ** ", "      **   ");
-		arr[6] = String.join(" ", "   ****   ", "   *****   ");
 		
 		for(String s : arr){
 			System.out.println(s);
