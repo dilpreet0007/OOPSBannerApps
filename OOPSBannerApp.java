a29b03a9f832a9516a1613a400a0422436dd4553
dilpreet0007 <dilpreet.sandhu2004@gmail.com>
2026-02-18 14:39:37 +0530

    UC2 code is added in UC2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,15 @@
-public class OOPSBannerApp{
+/*
+	Feature UC2 - Print OOPS
+	 @auther Developer
+	 @version 2.0
+
+*/
+
+public class OOPSBannerApp2{
 	public static void main(String[] args){
-		System.out.println("OOPS");
+		System.out.println(" **  " + " **  " + "**** " + "**** ");
+		System.out.println("*  * " + "*  * " + "*  * " + "**   ");
+		System.out.println("*  * " + "*  * " + "**** " + "  ** ");
+		System.out.println(" **  " + " **  " + "*    " + "**** ");
 	}	
 } 
